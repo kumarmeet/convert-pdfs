6655218d86b2cca947b310c5c3566fe20c0d71b1
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2023-07-26 12:36:12 +0530

    Update downloadFileFromS3.js
    update s3 object and upload data from local server to s3

--- downloadFileFromS3.js
+++ downloadFileFromS3.js
@@ -1,27 +1,54 @@
-const fs = require("fs")
-const AWS = require("aws-sdk")
-require("aws-sdk/lib/maintenance_mode_message").suppress = true // using when use to javascript
+const fs = require("fs");
+const AWS = require("aws-sdk");
+require("aws-sdk/lib/maintenance_mode_message").suppress = true; // using when use to javascript
 
 AWS.config.update({
     credentials: {
         accessKeyId: process.env.S3_KEY,
         secretAccessKey: process.env.S3_SECRET,
     },
-})
+    region: process.env.S3_REGION,
+});
 
 async function downloadFileFromS3(bucketName, key, localPath) {
-    const s3 = new AWS.S3()
+    const s3 = new AWS.S3();
     const params = {
         Bucket: bucketName,
         Key: key,
-    }
+    };
 
-    const { Body } = await s3.getObject(params).promise()
-    fs.writeFileSync(localPath, Body, "utf8")
+    const { Body } = await s3.getObject(params).promise();
+    fs.writeFileSync(localPath, Body, "utf8");
 
-    console.log(`Downloaded PDF file: ${localPath}`)
+    console.log(`Downloaded PDF file: ${localPath}`);
 }
+
+const updateS3Object = async (bucketName, objectKey, updatedContent) => {
+    const s3 = new AWS.S3();
+
+    fs.readFile(updatedContent, async (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            return;
+        }
+
+        const params = {
+            Bucket: bucketName,
+            Key: objectKey,
+            Body: data,
+            ACL: "public-read",
+        };
+
+        try {
+            await s3.putObject(params).promise();
+            console.log(`Object with key "${objectKey}" updated successfully.`);
+        } catch (error) {
+            console.error("Error updating object:", error);
+        }
+    });
+};
 
 module.exports = {
     downloadFileFromS3,
-}
+    updateS3Object,
+};
